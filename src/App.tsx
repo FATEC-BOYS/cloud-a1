06e6ab40560f456cd9320b3c00cf208aac17545d
unknown <als.sanches@outlook.com>
2025-09-27 21:33:52 -0300

    fix meu deus

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import RealtimeSpatialAnalysis from "./RealtimeSpatialAnalysis";
 
 function App() {
   <div className="p-4 bg-emerald-600 text-white rounded-xl">
-  Se você vê este bloco verde, o Tailwind está ok ✅
 </div>
 
   return <RealtimeSpatialAnalysis />;
